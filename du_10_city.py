496aa6f91b00f6057f2ebaba046c0a77d5b3ce94
nspssup <nspssup@gmail.com>
2025-02-11 12:24:00 +0100

    change to another

--- du_10_city.py
+++ du_10_city.py
@@ -18,7 +18,20 @@ zipcode: {self.zipcode}
 areacode: {self.areacode}
               """)
 
-this_city = City(name = "IpD", region="BSK", country="SK",citizens="10000",zipcode="90028",areacode=None)
+class otherCity(City):
+    def __init__(self, name, region, country, citizens, zipcode, areacode, pubs):
+        super.__init__(name, region, country, citizens, zipcode, areacode)
+        self.pubs = pubs
+
+        def get_props(self):
+            return super().get_props() + (self.pubs)
+
+this_city = City(name = "IpD", region="BSK", country="SK",citizens=10000,zipcode=90028,areacode=None)
+HornaDolna = otherCity(name = "HornaDolna", region="unknown", country="SK",citizens=5.5,zipcode=12345, areacode=None, pubs=1 )
+
+
 
 print(this_city.get_props())
 this_city.print_props()
+print(HornaDolna.__getattribute__("pubs"))
+
